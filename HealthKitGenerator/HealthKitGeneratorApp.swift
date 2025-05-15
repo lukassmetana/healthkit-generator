72db6056ac3fa19755fb87216cd60551d2642b37
Lukas Smetana <lukas.smetana.ls@gmail.com>
2025-05-15 10:57:38 +0200

    Initial Commit

---
+++ HealthKitGenerator/HealthKitGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitGeneratorApp.swift
+//  HealthKitGenerator
+//
+//  Created by Lukas Smetana on 15.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
